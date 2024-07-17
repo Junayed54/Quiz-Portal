f0a43bb6387dbe6f6b1a63edda09acecdfa36ad1
Junayed-shanto <junayedshanto54@gmail.com>
2024-07-17 20:56:40 +0600

    edit .sh file

--- build_files.sh
+++ build_files.sh
@@ -5,11 +5,8 @@ set -e
 
 echo "BUILD START"
 
-# Activate your virtual environment if it's not already activated
-source /path/to/your/venv/bin/activate  # Replace with the actual path to your virtual environment
-
 # Install requirements
-pip3 install -r requirements.txt
+python3.9 -m pip3 install -r requirements.txt
 
 # Collect static files
 python3.9 manage.py collectstatic --noinput
