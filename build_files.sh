22ad77a8e567ee972628863876792b344934d615
Junayed-shanto <junayedshanto54@gmail.com>
2024-07-17 18:49:56 +0600

    edit build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,2 @@
-#!/bin/bash
-
-# Ensure the script exits on any error
-set -e
-
-echo "BUILD START"
-
-# Install requirements
-python3.9 -m pip install -r requirements.txt
-
-# Create the staticfiles_build directory if it doesn't exist
-mkdir -p staticfiles_build
-
-# Collect static files to the staticfiles_build directory
-python3.9 manage.py collectstatic --noinput --clear --directory staticfiles_build
-
-echo "BUILD END"
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
