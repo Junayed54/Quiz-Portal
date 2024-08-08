860d349dbf3e2ad33fca3bda8266b33c63a0f44f
Junayed-shanto <junayedshanto54@gmail.com>
2024-08-08 23:41:11 +0600

    Add postgress with this project

--- quiz_portal/settings.py
+++ quiz_portal/settings.py
@@ -96,21 +96,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'quiz_portal.wsgi.application'
 
 # Database configuration (SQLite for simplicity)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# DATABASE_URL = 'postgresql://postgres:ZdmqgHnUEPmcAIDHQOCwYRxjERQxfrnT@viaduct.proxy.rlwy.net:40668/railway'
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL', default=DATABASE_URL)
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASE_URL = 'postgresql://postgres:JcfkbcGFQCcpnvUPdoMsRccFJHujpQpi@roundhouse.proxy.rlwy.net:44568/railway'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=DATABASE_URL)
+    )
+}
 
 
 # Password validation
